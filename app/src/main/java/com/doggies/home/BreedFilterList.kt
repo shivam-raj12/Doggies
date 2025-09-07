d2491a4c8badd66450d379aeaa39bf5f65436161
Shivam <157960855+shivam-raj12@users.noreply.github.com>
2025-09-07 20:19:39 +0530

    Update IntelliJ configuration files and AGP version

--- app/src/main/java/com/doggies/home/BreedFilterList.kt
+++ app/src/main/java/com/doggies/home/BreedFilterList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 
 @Composable
--- app/src/main/java/com/doggies/repository/Doggies.kt
+++ app/src/main/java/com/doggies/repository/Doggies.kt
@@ -7,7 +7,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
